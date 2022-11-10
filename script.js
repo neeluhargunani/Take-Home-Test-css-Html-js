5c3067eb1757c1565e99f150da32383b85d239c0
neelu <neelamh1000@gmail.com>
2022-11-11 00:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+//script
+let mainNav=document.getElementById("js-menu"),navBarToggle=document.getElementById("js-navbar-toggle");navBarToggle.addEventListener("click",function(){mainNav.classList.toggle("active")});
